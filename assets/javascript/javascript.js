331e0aacfcc73a8bdc22c17492fc75b23b49653c
Logan Moss <lrdjmoss@gmail.com>
2020-01-19 15:46:16 -0800

    added beginning framework for homework

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,16 @@
+  
+  
+  
+  
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCKDcachb6ml-mURyMAIfQP_06RMM2Yy1I",
+    authDomain: "rps-multiplayer-b7980.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-b7980.firebaseio.com",
+    projectId: "rps-multiplayer-b7980",
+    storageBucket: "rps-multiplayer-b7980.appspot.com",
+    messagingSenderId: "825174842638",
+    appId: "1:825174842638:web:100d478dc9e6b448b6cb5f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
